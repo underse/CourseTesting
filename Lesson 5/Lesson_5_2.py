34bb6b13d9445e5ecba1422bed21fb0dc681aaef
underse <p-g@bk.ru>
2022-06-28 14:17:09 +0300

    Задача 3.

--- Lesson 5/Lesson_5_2.py
+++ Lesson 5/Lesson_5_2.py
@@ -4,7 +4,18 @@
 
 
 def main():
+    filename = 'Lesson_5_2.txt'
+    print(f'Файл: {filename}')
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        line_num = 0
+        for line in lines:
+            line_num += 1
+            words = line.split(' ')
+            words_count = len(words)
+            print(f'Строка: {line_num}. Слов: {words_count}')
 
+    print(f'Строк: {len(lines)}')
 
 
 if __name__ == '__main__':
--- Lesson 5/Lesson_5_3.py
+++ Lesson 5/Lesson_5_3.py
@@ -6,8 +6,22 @@
 Петров 13749.32
 """
 
-def main():
 
+def main():
+    filename = 'Lesson_5_3_personal.txt'
+    avg = 0
+    fund = 0
+    personal_count = 0
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            surname, salary = line.strip().split(' ')
+            salary = float(salary)
+            fund += salary
+            personal_count += 1
+            if salary < 20000:
+                print(f'{surname} <20000.00 ({salary:.2f})')
+
+    print(f'Средний доход: {fund/personal_count:.2f}')
 
 
 if __name__ == '__main__':
