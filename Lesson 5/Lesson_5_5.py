0246e60cdf164ba557717cd4034e2629634f5227
underse <p-g@bk.ru>
2022-06-28 15:40:10 +0300

    Задание 5.

--- Lesson 5/Lesson_5_5.py
+++ Lesson 5/Lesson_5_5.py
@@ -5,7 +5,21 @@
 
 
 def main():
+    filename = 'Lesson_5_5.txt'
 
+    s = input('Введите числа разделённые пробелами: ')
+    digits_list = s.split(' ')
+
+    with open(filename, 'w+', encoding='utf-8') as f:
+        f.write(s)
+
+    print(f'Введённые числа: {digits_list} записаны в файл: {filename}')
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        digits_list2 = [float(x) for x in f.read().split(' ')]
+        print(f'Прочитаны из файла: {filename} числа: {digits_list2}')
+        digits_sum = sum(digits_list2)
+        print(f'Сумма чисел из файла {filename}: {digits_sum}')
 
 
 if __name__ == '__main__':
