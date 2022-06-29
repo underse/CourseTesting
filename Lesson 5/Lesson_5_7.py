4f23616c873af41980be8658497a1e2ee2b95fd5
underse <p-g@bk.ru>
2022-06-29 20:28:44 +0300

    Урок 5.7. Решение задачи 7.

--- Lesson 5/Lesson_5_7.py
+++ Lesson 5/Lesson_5_7.py
@@ -11,10 +11,46 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+import json
 
 
 def main():
+    filename = 'Lesson_5_7.txt'
+    firms_dict = {}
+    result = []
+    average_profit = {}
+    firm_counter = 0
+    all_revenue = 0
+    with open(filename, encoding='utf-8') as f:
+        for line in f.readlines():
+            try:
+                if line.startswith('#'):
+                    continue
+                firm_counter += 1
+                name, form, gain, costs = line.strip().split(' ')
+                print(name, form, gain, costs)
+                gain = int(gain)
+                costs = int(costs)
+                firm_revenue = gain - costs
+                print(f'Прибыль: {form} {name}: {firm_revenue}')
 
+                if firm_revenue >= 0:
+                    all_revenue += firm_revenue
+                firms_dict[name] = firm_revenue
+                print()
+            except ValueError as e:
+                print('\nОшибка чтения из файла! Проверьте правильность исходного файла.\nДолжно быть четыре поля в каждой строке.\nНапример: firm_1 ООО 10000 5000')
+    avg_profit = firm_revenue / firm_counter
+    print(f'Средняя прибыль по {firm_counter} компаниям: {avg_profit}')
+
+    print(firms_dict)
+    result.append(firms_dict)
+    average_profit['average_profit'] = avg_profit
+    result.append(average_profit)
+    print(result)
+
+    with open('Lesson_5_7.json', 'w+') as f:
+        json.dump(result, f)
 
 
 if __name__ == '__main__':
